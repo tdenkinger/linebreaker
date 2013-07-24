4beef34806b1764e35181227fa3f17f702540070
Troy Denkinger <tdenkinger@gmail.com>
2013-07-24 11:33:36 -0400

    Simplest pieces done.

--- spec/linebreaker_spec.rb
+++ spec/linebreaker_spec.rb
@@ -1,7 +1,41 @@
 require 'rspec'
 
+class Linebreaker
+  def self.break line, breakpoint
+    return "" if line.nil?
+    return line if line.length <= breakpoint
+    splitup(line, breakpoint)
+  end
+
+private
+
+  def self.splitup(line, breakpoint)
+    line.scan(/.{#{breakpoint}}|.+/).join '\n'
+  end
+end
+
 describe "Linebreaker", "A routine to break lines" do
-  it "works" do
-    expect(true).to eq true
+  it "handles nil content" do
+    expect(Linebreaker.break nil, 1 ).to eq ""
+  end
+
+  it "handles an empty line" do
+    expect(Linebreaker.break "", 1 ).to eq ""
+  end
+
+  it "handles a single character line" do
+    expect(Linebreaker.break "a", 1).to eq "a"
+  end
+
+  it "handles a line with 2 characters" do
+    expect(Linebreaker.break "ab", 1).to eq 'a\nb'
+  end
+
+  it "handles a line with 3 characters" do
+    expect(Linebreaker.break "abc", 2).to eq 'ab\nc'
+  end
+
+  xit "handles words" do
+    expect(Linebreaker.break "four score", 5).to eq 'four\nscore'
   end
 end
